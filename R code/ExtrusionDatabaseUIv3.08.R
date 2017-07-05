e904d9a85481e12f90163376d6053f14762efa40
lingjiekong91 <lingjie.kong@bsci.com>
2017-07-05 13:17:45 -0500

    A made a final change

--- R code/ExtrusionDatabaseUIv3.08.R
+++ R code/ExtrusionDatabaseUIv3.08.R
@@ -13,7 +13,9 @@ library(jpeg)
 library(ggplot2)
 library(DT)
 
-#B made a test, 1+1=2,2+2=4
+
+#B made a test,2+2=4
+
 
 #Creating variables across all sessions
 path <- "C:/Users/correaf/Downloads/PPS_document_contents/ListOfSheets/GTE"

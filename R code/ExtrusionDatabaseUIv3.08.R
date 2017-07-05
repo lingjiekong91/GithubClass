860b6a818f34f38b40f850c30b6e7f2c7623ca18
lingjiekong91 <lingjie.kong@bsci.com>
2017-07-05 15:08:25 -0500

    Update ExtrusionDatabaseUIv3.08.R

--- R code/ExtrusionDatabaseUIv3.08.R
+++ R code/ExtrusionDatabaseUIv3.08.R
@@ -5,7 +5,7 @@
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
-#Change the master branch directly
+#Change the master branch
 
 library(shiny)
 library(bootstrap)
